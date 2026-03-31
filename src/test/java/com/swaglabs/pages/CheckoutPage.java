b0a55512fb87f4b066dc44ea0340684cab589b47
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-03-31 13:03:49 +0200

    feat: add wait for ITEM_TOTAL visibility after filling checkout form for improved reliability

--- src/test/java/com/swaglabs/pages/CheckoutPage.java
+++ src/test/java/com/swaglabs/pages/CheckoutPage.java
@@ -39,6 +39,7 @@ public class CheckoutPage extends BasePage {
         type(LAST_NAME_INPUT, lastName);
         type(POSTAL_CODE, postalCode);
         submitFormViaJs();
+        waitForElementVisible(ITEM_TOTAL);
         waitForElementPresent(FINISH_BUTTON);
         return this;
     }
